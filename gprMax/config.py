90c8d0d663f7a36dc2a61fc5dec6e1f447c64a89
John Hartley <johnmatthewhartley@gmail.com>
2019-07-16 16:14:48 +0100

    hsg updates

---
+++ gprMax/config.py
@@ -0,0 +1,7 @@
+# degree of the spline used to interpolate fields to points in the subgrid
+# can be 1, 2, 3, 4
+sg_interp_d = 3
+# use the berenger cascading filter. Using the filter slightly reduces the
+# accuracy of the simulation. However it is recommended as a large in increase
+# in stability is observed.
+filter = True
